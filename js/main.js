4d5c8248ddb08fbd63fae5d302937c54d67208c5
Mark Koester <mwkteacher@gmail.com>
2011-11-01 22:18:24 +0800

    another tweak for dynamic list

--- js/main.js
+++ js/main.js
@@ -24,13 +24,6 @@ $(function() {
           $("<td>").addClass("addon_name").addClass("no_wrap").text(item.name).appendTo(row)
           $("<td>").addClass("addon_desc").text(item.description.substring(12)).appendTo(row)
           var last_cell = $("<td>").addClass("addon_links").addClass("no_wrap").addClass("right-text").addClass("padded_links").appendTo(row)
-          if (item.pledgie) {last_cell.append($("<a>").text("Donate").attr("href", "http://pledgie.org/campaigns/" + item.pledgie))}
-
-          var buglink = $("<a>").attr("id", "bugs").text("Bugs (0)").attr("href", item.url + "/issues")
-          if (item.open_issues > 0) {
-            buglink.addClass("has_issues").text("Bugs (" + item.open_issues + ")")
-          }
-          last_cell.append(buglink)
 
           last_cell.append($("<a>").text("Repo").attr("href", item.url))
           $("#addon_list").append(row)
